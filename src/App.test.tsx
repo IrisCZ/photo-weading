009c4250e86ad103a391e3a0961c8d4ca283e8d1
IrisCZ <iriscarballo@gmail.com>
2022-03-23 10:36:15 +0100

    Refactor

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders welcome text', () => {
-  render(<App />);
-  const welcomeElement = screen.getByText(/bienvenidos a/i);
-  expect(welcomeElement).toBeInTheDocument();
-});
--- src/Welcome.tsx
+++ src/Welcome.tsx
@@ -1,12 +1,12 @@
 import "./App.css";
 import { useState } from "react";
 
-type Props = {
-  onSendInfo: ({ name, email }: { name: string; email: string }) => void;
-  organizer: string
+type WelcomeProps = {
+  onSubmitInfo: ({ name, email }: { name: string; email: string }) => void;
+  organizer: string;
 };
 
-function Welcome({ onSendInfo, organizer }: Props) {
+function Welcome({ onSubmitInfo, organizer }: WelcomeProps) {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
 
@@ -17,42 +17,42 @@ function Welcome({ onSendInfo, organizer }: Props) {
         <h1>{organizer}</h1>
       </header>
       <section className="form-container">
-        <form action="" method="get">
-          <div className="form-field">
-            <label htmlFor="name">
-              Dinos tu nombre para que sepamos de quién son las fotos.
-            </label>
-            <input
-              type="text"
-              className="form-input"
-              name="name"
-              value={name}
-              id="name"
-              required
-              onChange={(e) => setName(e.target.value)}
-            />
-          </div>
-          <div className="form-field">
-            <label htmlFor="email">
-              Dinos tu email para que podamos mandarte las fotos dentro de unos días. <br /> ¡No seais impacientes!
-            </label>
-            <input
-              type="text"
-              className="form-input"
-              name="email"
-              value={email}
-              id="email"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <button
-            id="snap"
-            className="button"
-            onClick={() => onSendInfo({ name, email })}
-          >
-            <i className="fa-solid fa-paper-plane"></i>
-          </button>
-        </form>
+        <div className="form-field">
+          <label htmlFor="name">
+            Dinos tu nombre para que sepamos de quién son las fotos.
+          </label>
+          <input
+            type="text"
+            className="form-input"
+            value={name}
+            id="name"
+            required
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className="form-field">
+          <label htmlFor="email">
+            Dinos tu email para que podamos mandarte las fotos dentro de unos
+            días. <br /> ¡No seais impacientes!
+          </label>
+          <input
+            type="text"
+            className="form-input"
+            value={email}
+            id="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <button
+          className="button"
+          onClick={(e) => {
+            e.stopPropagation();
+            onSubmitInfo({ name, email });
+          }}
+          aria-label="submit"
+        >
+          <i className="fa-solid fa-paper-plane"></i>
+        </button>
       </section>
     </div>
   );
