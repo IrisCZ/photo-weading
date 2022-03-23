a791fdcedb1ed55b1b22bd7e7b6785f5aba07b0e
IrisCZ <iriscarballo@gmail.com>
2022-03-23 10:21:29 +0100

    Add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
 import "./App.css";
+import "./images/saveTheDate.jpeg";
+import { ReactComponent as Loading } from "./images/spinner-1s-200px.svg";
 import Camera from "./Camera";
 import Welcome from "./Welcome";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { useLocalStorage } from "./useLocalStorage";
+import { useEffect, useState } from "react";
 
 function App() {
+  return (
+    <main className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={UserCamera()} />
+        </Routes>
+      </Router>
+    </main>
+  );
+}
+export const UserCamera = () => {
   const organizer = "Sara y Celes";
   const [name, setName] = useLocalStorage("name", "");
   const [, setEmail] = useLocalStorage("email", "");
-  const handleSend = ({
+
+  const handleSubmit = ({
     name: newName,
     email: newEmail,
   }: {
@@ -20,37 +36,33 @@ function App() {
 
   const handleUploadPhoto = (photoSrc: Blob) => {
     fetch(`/new-photo-link?name=${name}`).then(async (result) => {
-      if (result.ok){
-        const {link: url} = (await result.json());
+      if (result.ok) {
+        const { link: url } = await result.json();
         fetch(url, {
-          method: 'PUT',
+          method: "PUT",
           headers: new Headers({
-            "Content-Type": 'image/jpeg'
+            "Content-Type": "image/jpeg",
           }),
-          body: photoSrc
-        }).then((response) => {
+          body: photoSrc,
+        })
+          .then((response) => {
           if (response.status === 200) {
-            console.log('ENVIADO!!!')
-          }
-        }).catch(() => {
-          alert("ERROR")
-        });
+              console.log("ENVIADO!!!");
       }
     })
+          .catch(() => {
+            alert("ERROR");
+          });
   }
+    });
+  };
 
-  return (
-    <main className="App">
-      {name ? (
+  return name ? (
         <Camera
           onUploadPhoto={handleUploadPhoto}
           organizer={organizer ? organizer : "el organizador de la boda"}
         />
       ) : (
-        <Welcome onSendInfo={handleSend} organizer={organizer} />
-      )}
-    </main>
+    <Welcome onSubmitInfo={handleSubmit} organizer={organizer} />
   );
-}
-
-export default App;
+};
